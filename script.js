a8c1325518dac9ac525e07d0c811ff8214e541e4
Buddhika Thanura <buddhikajayasingha@gmail.com>
2023-07-21 15:34:15 +0530

    Fixing bugs
    - Fixed not adding strikethrough font style when tasks are checked
    - Fixed bulk delete issue right after adding tasks
    - Added new functionality to show the date
    - Changed Add and Delete buttons' positions.

--- script.js
+++ script.js
@@ -28,7 +28,28 @@ if (currentHour >= 0 && currentHour < 4) {
   greetingMessage.textContent = "Good Night!";
 }
 
-//---------------------------------------------------------------------------
+// Showing the date
+const today = document.getElementById("today");
+const dayName = document.getElementById("date");
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+const day = now.getDay();
+const year = now.getFullYear();
+const month = now.getMonth();
+const date = now.getDate();
+
+today.innerHTML = `Today's ${days[day]}`;
+dayName.innerHTML = `${months[month]} ${date}, ${year}`;
+
+
+//--------------------------------------------------------------------------------
+
+
+
+
+//--------------------------- OTHER FUNCTIONS ------------------------------------
 
 //----------- Function to show or hide empty tasks message ------------------
 
@@ -49,8 +70,9 @@ function showNoTasksMessage() {
     // noTasksImage.classList.remove("hidden");
   }
 }
-
 //---------------------------------------------------------------------------------
+
+
 
 //----------- Function to show or hide task input overlay modal ------------------
 
@@ -68,6 +90,7 @@ const openModal = function (e) {
 // Close the modal when the close button is clicked
 const closeModal = function () {
   modal.classList.add("hidden");
+  window.location.reload(true);
 };
 
 // Close the modal when the escape key is pressed
@@ -92,10 +115,69 @@ btnCloseModal.addEventListener("click", closeModal);
 //---------------------------------------------------------------------------------
 
 
-// Adding new tasks
 
-// // Get the task list element from HTML
-// const taskList = document.getElementById("task-list");
+//----------- Functions related to Delete button ------------------------------------
+
+// Change the style of the delete button when it is clicked
+const deleteBtnChange = function () {
+  deleteButton.classList.toggle("delete-button-clicked");
+};
+
+
+// Add event listener to delete button
+const deleteButton = document.querySelector(".delete-button");
+deleteButton.addEventListener("click", deleteCheckedTasks);
+
+//---------------------------------------------------------------------------------
+
+
+
+
+//----------- Functions related to Local Storage ------------------------------------
+
+// Function to display tasks from local storage on page load
+function displayTasks() {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach((task) => {
+    const taskListItem = document.createElement("li");
+    if (task.striked) {
+      taskListItem.classList.add("strike");
+    }
+    taskListItem.innerHTML = `
+      <input type="checkbox" id="${task.id}" class="task-checkbox" ${
+      task.completed ? "checked" : ""
+    } />
+      <label for="${task.id}" class="task-text">${task.text}</label>
+    `;
+    taskList.appendChild(taskListItem);
+  });
+}
+
+
+// Function to update local storage
+function updateLocalStorage() {
+  // Get all the task items from the DOM
+  const taskItems = taskList.querySelectorAll("li");
+
+  // Create a new array of task objects from the task items
+  const tasks = Array.from(taskItems).map((taskItem) => {
+    const checkbox = taskItem.querySelector(".task-checkbox");
+    const id = checkbox.getAttribute("id");
+    const text = taskItem.querySelector(".task-text").textContent;
+    const completed = checkbox.checked; 
+    const striked = taskItem.classList.contains("strike");
+    return { id, text, completed, striked };
+  });
+
+  // Update the tasks in local storage
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+//---------------------------------------------------------------------------------
+
+
+//-------------------- ADDING NEW TASKS -------------------------------------------
+
 
 // Function to add a new task to the task list
 function addTask(event) {
@@ -113,6 +195,7 @@ function addTask(event) {
       id: Math.floor(Math.random() * 1000000),
       text: newTaskText,
       completed: false,
+      striked: false,
     };
 
     // Add the new task to local storage
@@ -129,81 +212,64 @@ function addTask(event) {
       `;
     taskList.appendChild(taskListItem);
 
-    // Add event listener to delete button
-    const deleteButton = taskListItem.querySelector(".delete-button");
-    deleteButton.addEventListener("click", () => {
-      taskListItem.remove();
-      updateLocalStorage();
-    });
+    // Displaying tasks as strikethrough font when checked 
+    const newCheckbox = taskListItem.querySelector(".task-checkbox");
+          newCheckbox.addEventListener("click", () => {
+          taskListItem.classList.toggle("strike");
+          updateLocalStorage();
+});
+
+
+    // Add event listener to the delete button of the task
+      taskListItem.querySelector(".delete-button").addEventListener('click', function() {
+        const checkbox = taskListItem.querySelector(".task-checkbox");
+        // Only delete the task if it is checked
+        if (checkbox.checked) {
+          taskListItem.remove(); // Remove the task from the list
+          tasks = tasks.filter(task => task.id !== newTask.id); // Remove the task from the array
+          localStorage.setItem("tasks", JSON.stringify(tasks)); // Update local storage
+        }
+      });
 
     // Reset the input field value
     newTaskInput.value = "";
   }
-  showNoTasksMessage()
+
+  showNoTasksMessage(); // Check if there are any tasks left and display the no tasks message if necessary
+  
 }
-
-// Function to delete checked tasks
-function deleteCheckedTasks() {
-  // Get all the checked checkboxes
-  const checkedCheckboxes = document.querySelectorAll(".task-checkbox:checked");
-
-  // Loop through the checked checkboxes and remove their parent li element
-  checkedCheckboxes.forEach((checkbox) => {
-    const taskListItem = checkbox.closest("li");
-    taskListItem.remove();
-    updateLocalStorage();
-  });
-}
-
-// Change the style of the delete button when it is clicked
-const deleteBtnChange = function () {
-  deleteButton.classList.toggle("delete-button-clicked");
-};
-
-// Add event listener to delete button
-const deleteButton = document.querySelector(".delete-button");
-deleteButton.addEventListener("click", deleteCheckedTasks);
 
 // Event listener to add a new task when the form is submitted
 const addTaskForm = document.querySelector("form");
 addTaskForm.addEventListener("submit", addTask);
 
-// Function to display tasks from local storage on page load
-function displayTasks() {
-  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  tasks.forEach((task) => {
-    const taskListItem = document.createElement("li");
-    taskListItem.innerHTML = `
-      <input type="checkbox" id="${task.id}" class="task-checkbox" ${
-      task.completed ? "checked" : ""
-    } />
-      <label for="${task.id}" class="task-text">${task.text}</label>
-    `;
-    taskList.appendChild(taskListItem);
-  });
-}
+//---------------------------------------------------------------------------------
 
-function updateLocalStorage() {
-  // Get all the task items from the DOM
-  const taskItems = taskList.querySelectorAll("li");
 
-  // Create a new array of task objects from the task items
-  const tasks = Array.from(taskItems).map((taskItem) => {
-    const checkbox = taskItem.querySelector(".task-checkbox");
-    const id = checkbox.getAttribute("id");
-    const text = taskItem.querySelector(".task-text").textContent;
-    const completed = checkbox.checked;
-    return { id, text, completed };
-  });
 
-  // Update the tasks in local storage
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+
+//-------------------- DELETE TASKS -------------------------------------------
+
 
 // Function to delete checked tasks
 function deleteCheckedTasks() {
   // Get all the checked checkboxes
   const checkedCheckboxes = document.querySelectorAll(".task-checkbox:checked");
+
+  // Reload tasks array from local storage
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  if (checkedCheckboxes.length > 0) {
+    // Animating delete button by replacing default icon with a gif
+    const originalSrc = deleteButton.querySelector("img").src;
+    const newSrc = "/images/icons8-waste-cleanbg.gif";
+
+    deleteButton.querySelector("img").src = newSrc;
+    setTimeout(() => {
+      deleteButton.querySelector("img").src = originalSrc;
+    }, 1000); // change back to original after 2 seconds
+  }
+
   // Loop through the checked checkboxes and remove their parent li element
   checkedCheckboxes.forEach((checkbox) => {
     const taskListItem = checkbox.closest("li");
@@ -222,32 +288,31 @@ function deleteCheckedTasks() {
 
   // Call the function to check if there are any tasks on page load
   showNoTasksMessage();
+
 }
+
+//---------------------------------------------------------------------------------
+
+
+
+//-------------------- Functions to run when page loads -------------------------------------------
+
 
 // Call the displayTasks function on page load
 displayTasks();
 
-// "Displaying tasks as strikethrough font when checked."
+// Call the function to check if there are any tasks on page load
+showNoTasksMessage();
+
+// Displaying tasks as strikethrough font when checked.
 const checkboxes = document.querySelectorAll(".task-checkbox");
 
 checkboxes.forEach((checkbox) => {
   checkbox.addEventListener("click", () => {
-    const taskListItem = checkbox.closest("li");
+    const taskListItem = checkbox.parentElement;
     taskListItem.classList.toggle("strike");
     updateLocalStorage();
   });
 });
 
-// Animating delete button by replacing default icon with a gif
-const originalSrc = deleteButton.querySelector("img").src;
-const newSrc = "/images/icons8-waste-cleanbg.gif";
-
-deleteButton.addEventListener("click", () => {
-  deleteButton.querySelector("img").src = newSrc;
-  setTimeout(() => {
-    deleteButton.querySelector("img").src = originalSrc;
-  }, 1000); // change back to original after 2 seconds
-});
-
-// Call the function to check if there are any tasks on page load
-showNoTasksMessage();
+//---------------------------------------------------------------------------------
